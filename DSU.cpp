bfbdaf54390acd089ddd8d0a55769f2817623c5e
Arindam Nag <83442974+arindam54321@users.noreply.github.com>
2021-08-16 13:54:56 +0530

    Disjoint Set Union

--- DSU.cpp
+++ DSU.cpp
@@ -13,7 +13,8 @@ public:
 
     int findpar(int n)
     {
-        if(n==parent[n]) return n;
+        if(n==parent[n]) 
+            return n;
         return parent[n]=findpar(parent[n]);
     }
     
