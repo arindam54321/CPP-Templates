2bb4c0aae20fe0ccd8126863bf3c007d04d7dfb7
Arindam Nag <83442974+arindam54321@users.noreply.github.com>
2021-08-16 19:21:13 +0530

    Seive
    To find prime numbers upto 10^7

---
+++ seivePrime.cpp
@@ -0,0 +1,23 @@
+vector<int> isprime(1e7+5,1); //prime or not?
+vector<int> prime; //all prime numbers
+vector<int> spf(1e7+5); //smallest prime factor
+
+void isPrimeSeive(int n)
+{
+	isprime[0]=isprime[1]=0;
+
+	for(int i=2;i<n;i=i+1)
+	{
+		if(isprime[i]==1)
+		{
+			prime.emplace_back(i);
+			spf[i]=i;
+		}
+
+		for(int j=0; j<prime.size() && i*prime[j]<n && prime[j]<=spf[i]; j=j+1)
+		{
+			isprime[i*prime[j]]=0;
+			spf[i*prime[j]]=prime[j];
+		}
+	}
+}
