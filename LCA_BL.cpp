ae31f6c4e1bb391e58ae7bfcca1f68ee0f2d0746
Arindam Nag <83442974+arindam54321@users.noreply.github.com>
2021-08-16 13:49:17 +0530

    Add files via upload
    Finding LCA ( Lowest Common Ancestor) using Binary-Lifting

---
+++ LCA_BL.cpp
@@ -0,0 +1,45 @@
+int N=100005,logg=20;
+vector<vector<int>> adj(N),lca(N,vector<int>(logg+1));
+vector<int> level(N);
+// Initialize level[] = 0 , lca[][] = -1
+
+void dfs(int n,int par)
+{
+    lca[n][0]=par;
+    for(int i=1;i<=logg;i++)
+        lca[n][i]=lca[lca[n][i-1]][i-1];
+        
+    for(auto it:adj[n])
+    {
+        if(it!=par)
+        {
+            level[it]=level[n]+1;
+            dfs(it,n);
+        }
+    }
+}
+
+int LCA(int u,int v)
+{
+    if(level[u]>level[v]) swap(u,v);
+    
+    for(int i=logg;i>=0;i--)
+    {
+        if(lca[v][i]!=-1&&level[lca[v][i]]>=level[u])
+            v=lca[v][i];
+    }
+    
+    if(u==v)
+        return u;
+    
+    for(int i=logg;i>=0;i--)
+    {
+        if(lca[u][i]!=lca[v][i])
+        {
+            u=lca[u][i];
+            v=lca[v][i];
+        }
+    }
+    
+    return lca[v][0];
+}
