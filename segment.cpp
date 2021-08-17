4ecba4744d6ac4915920cfa95614fc68fee8f581
Arindam Nag <83442974+arindam54321@users.noreply.github.com>
2021-08-17 20:59:53 +0530

    Segment Tree
    Range Queries...

---
+++ segment.cpp
@@ -0,0 +1,48 @@
+class segment{
+public:
+    int N;
+    vector<int> a,seg;
+    
+    void init(int n)
+    {
+        N=n;
+        a.resize(N);
+        seg.resize(4*N);
+    }
+    
+    void build(int i,int low,int high)
+    {
+        if(low==high)
+        {
+            seg[i]=a[low];
+            return;
+        }
+        int mid=(low+high)/2;
+        build(i*2+1,low,mid);
+        build(i*2+2,mid+1,high);
+        
+        // depends on question
+        seg[i]=min(seg[i*2+1],seg[i*2+2]);
+    }
+    
+    int query(int i,int low,int high,int from,int to)
+    {
+        if(from<=low && to>=high)
+        {
+            return seg[i];
+        }
+        else if(from>high || to<low)
+        {
+            // depends on question
+            return INT_MAX;
+        }
+        else
+        {
+            int mid=(low+high)/2;
+            int left=query(i*2+1,low,mid,from,to);
+            int right=query(i*2+2,mid+1,high,from,to);
+            // depends on question
+            return min(left,right);
+        }
+    }
+};
