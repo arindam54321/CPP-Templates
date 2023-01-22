eb73a987ac48b503a48d5359e466c10c8f8f4388
Arindam Nag <83442974+arindam54321@users.noreply.github.com>
2023-01-22 22:04:55 +0530

    Update seivePrime.cpp

--- seivePrime.cpp
+++ seivePrime.cpp
@@ -1,23 +1,19 @@
-vector<int> isprime(1e7+5,1); //prime or not?
-vector<int> prime; //all prime numbers
-vector<int> spf(1e7+5); //smallest prime factor
+vector<int> isprime(1e7 + 5, 1); // prime or not?
+vector<int> prime; // all prime numbers
+vector<int> spf(1e7 + 5); // smallest prime factor
 
-void isPrimeSeive(int n)
-{
-	isprime[0]=isprime[1]=0;
+void isPrimeSeive(int n) {
+    isprime[0] = isprime[1] = 0;
 
-	for(int i=2;i<n;i=i+1)
-	{
-		if(isprime[i]==1)
-		{
-			prime.emplace_back(i);
-			spf[i]=i;
-		}
+    for (int i = 2; i < n; i = i + 1) {
+        if (isprime[i] == 1) {
+            prime.emplace_back(i);
+            spf[i] = i;
+        }
 
-		for(int j=0; j<prime.size() && i*prime[j]<n && prime[j]<=spf[i]; j=j+1)
-		{
-			isprime[i*prime[j]]=0;
-			spf[i*prime[j]]=prime[j];
-		}
-	}
+        for (int j = 0; j < prime.size() && i * prime[j] < n && prime[j] <= spf[i]; j = j + 1) {
+            isprime[i * prime[j]] = 0;
+            spf[i * prime[j]] = prime[j];
+        }
+    }
 }
