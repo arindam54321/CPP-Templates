1ed6363fd9e5b3b5421beb914dcf650b144e81cc
Arindam Nag <83442974+arindam54321@users.noreply.github.com>
2023-01-22 22:08:44 +0530

    Disjoint Set Union

--- DSU.cpp
+++ DSU.cpp
@@ -1,36 +1,30 @@
 class DSU {
-public:
-    vector<int> parent,rankk;
-    
-    void make_set(int n)
-    {
-        for(int i=0;i<n;i++)
-        {
+    public: vector<int> parent, rankk;
+
+    void make_set(int n) {
+        for (int i = 0; i < n; i++) {
             parent.emplace_back(i);
             rankk.emplace_back(0);
         }
     }
 
-    int findpar(int n)
-    {
-        if(n==parent[n]) 
+    int findpar(int n) {
+        if (n == parent[n])
             return n;
-        return parent[n]=findpar(parent[n]);
+        return parent[n] = findpar(parent[n]);
     }
-    
-    void unite(int u,int v)
-    {
-        u=findpar(u);
-        v=findpar(v);
-        
-        if(rankk[u]<rankk[v]) 
-            parent[u]=v;
-        else if(rankk[v]<rankk[u]) 
-            parent[v]=u;
-        else
-        {
-            parent[v]=u;
-            rankk[u]+=1;
+
+    void unite(int u, int v) {
+        u = findpar(u);
+        v = findpar(v);
+
+        if (rankk[u] < rankk[v])
+            parent[u] = v;
+        else if (rankk[v] < rankk[u])
+            parent[v] = u;
+        else {
+            parent[v] = u;
+            rankk[u] += 1;
         }
     }
 };
